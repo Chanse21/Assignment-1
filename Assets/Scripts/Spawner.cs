363a6f6ac6b5da53d4833675b899b90994b76148
Chanse21 <47192175+Chanse21@users.noreply.github.com>
2025-02-14 20:13:59 -0500

    Added new objects

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject EnemyPrefab;
-
+    public Transform [] spawnLocation;
     public void SpawnEnemy()
     {
-        Vector2 spawnPostion = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
-        Instantiate(EnemyPrefab, spawnPostion, Quaternion.identity);
+        Vector2 spawnLocation = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        Instantiate(EnemyPrefab, spawnLocation, Quaternion.identity);
     }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,22 +4,27 @@ using UnityEngine.Events;
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
-    public TMP_Text text;
-    public float timer = 5;
-    public AudioSource audiosource;
-    public AudioClip audioclip;
+    public float timerDuration = 5;
+    private float remainingTime;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
     public UnityEvent OnTimerElapse;
-    private bool isElapsed = false;
 
-
+    private void Start()
+    {
+        remainingTime = timerDuration;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
-        if (timer <= 5)
+        remainingTime -= Time.deltaTime;
+        timerText.text = remainingTime.ToString("F2");
+        if (remainingTime <= 0)
         {
             OnTimerElapse.Invoke();
+            remainingTime = timerDuration;
+            audioSource.PlayOneShot(audioClip);
         }
     }
 }
