989aee9870773de1cf80dc5779c4d7ea5a4d9aa1
Chanse21 <47192175+Chanse21@users.noreply.github.com>
2025-02-20 18:22:13 -0500

    Update Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 using TMPro;
 public class GameManager : MonoBehaviour
 {
-    public GameManager player;
+    public static GameManager Instance;
+    public PlayerController player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Instance = this;
     }
 
     // Update is called once per frame
